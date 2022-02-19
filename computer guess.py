8cc2c46e6135f00174f4aff66dba88b7c9c331e9
Khushi Pant <99333620+Lmaokhushi@users.noreply.github.com>
2022-02-19 20:16:28 +0530

    Add files via upload

--- computer guess.py
+++ computer guess.py
@@ -1,21 +1,20 @@
 import random
 
-def computer_guess(x):
-    low = 1
-    high = x
-    reply = ''
-    while reply != 'c':
-        if low != high:
-            guess = random.randint(low, high)
-        else:
-            guess = low
-        reply = input(f'Is the {guess} high (type h), low(type l), or correct (type c)')
-        if reply == 'h':
-            high = guess - 1
-        elif reply == 'l':
-            low = guess + 1
-        elif reply == 'c':
-            break
-    print("you have guessed the number correctly")
+x = int(input("Enter the number that you want the computer to guess out of : "))
 
-computer_guess(10)
+c = int(input(f"Enter a number between 1 to {x} that you want the computer to guess : "))
+
+def computer_guess(x):
+    for y in range(1, x):
+       guess = random.randint(1, x)
+       if guess == c:
+        quit()
+       elif guess > c:
+        print(f"Sorry, my guess {guess} is too high")
+        continue
+       elif guess < c:
+        print(f"Sorry, my guess {guess} is too low")
+        continue
+    print(f"The computer has guessed the number {c} correctly!!")
+
+computer_guess(x)
